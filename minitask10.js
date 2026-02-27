ac9ae15f8840c77b300dc284813a8a1a8563ea2f
M. Hanif Irfan <mhirfan173@gmail.com>
2026-02-27 11:03:21 +0700

    Add files via upload

---
+++ minitask10.js
@@ -0,0 +1,13 @@
+//string method concat
+let text1 = "Hello"
+let text2 = "World"
+let text3 = text1.concat(" ",text2)
+
+console.log(text3);
+
+//manual procces
+let texta = "Hello"
+let textb = "World"
+let textc = texta + " " + textb
+
+console.log(textc)
