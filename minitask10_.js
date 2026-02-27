29a7101bfa68444dd94722a2b0efd4622751e523
M. Hanif Irfan <mhirfan173@gmail.com>
2026-02-27 11:30:53 +0700

    Add files via upload

---
+++ minitask10_.js
@@ -0,0 +1,10 @@
+//pop()
+//let fruits = ["Apple", "Banana", "Cherry", "Durian"]
+//fruits.pop();
+//console.log(fruits);
+
+//manual
+let fruits = ["Apple", "Banana", "Cherry", "Durian"]
+fruits.length = fruits.length - 1
+console.log(fruits)
+
